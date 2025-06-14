f4d4acaf050dbd6ecff0329297bc1d5c284a69b0
DouglasTeyh <contato.douglasteyh@gmail.com>
2025-06-14 12:08:30 -0300

    inserção da navbar das paginas

---
+++ source/scripts/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("components/navbar.html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("navbar-placeholder").innerHTML = data;
+            const link = document.getElementById("orcamento-btn");
+            if (link) {
+                if (window.location.pathname.endsWith("orcamento.html")) {
+                    link.textContent = "Voltar";
+                    link.href = "index.html";
+                } else {
+                    link.textContent = "Fazer Orçamento";
+                    link.href = "orcamento.html";
+                }
+            }
+        });
+});
